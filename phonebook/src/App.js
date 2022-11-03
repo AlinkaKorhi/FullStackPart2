b159bb0387d529b1ce2ce1a86422a98c7af83d9b
unknown <korhi1536@gmail.com>
2022-11-03 21:08:16 +0400

    finish 2.11 task

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -1,18 +1,26 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Persons from './Persons'
 import Filter from './Filter'
 import PersonForm from './PersonForm'
 
 function App() {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filter, setNewFilter] = useState('')
+
+  function getPersonFromServer(){
+    const eventHandler = response => {
+      console.log('promise fulfilled')
+      setPersons(response.data)
+    }
+
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }
+
+  useEffect(getPersonFromServer,[])
 
   function validatePersonName(name){
     let result = true
